1f07461d2c73de81f3ce82945f4c811b89b48b10
anondobormon <anondokpi@gmail.com>
2021-04-02 00:05:47 +0600

    css update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.eklps.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eklps.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
 const pass = 'anondo123456789'
